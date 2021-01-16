a2fa6f847927b4b313ccdf8943ac8bf0173b3665
Shivam <shivam1634.cse18@chitkara.edu.in>
2021-01-16 23:02:21 +0530

    Create Inserting Node at given Position.java

---
+++ Arrays/Inserting Node at given Position.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Insertion {
+    public static void main(String[] args) {
+        /*Inserting element at given index in array */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n+1];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int pos,ele;
+        pos=sc.nextInt();
+        ele=sc.nextInt();
+        for(int i=(n-1);i>=(pos);i--)
+        {
+            a[i+1]=a[i];
+        }
+        a[pos]=ele;
+        for(int i=0;i<n+1;i++)
+        {
+            System.out.print(a[i]+ " ");
+        }
+        sc.close();
+}
+}
