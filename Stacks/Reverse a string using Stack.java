2ecf0051d7d3ace049e7b62ce789364eec02dc46
Shivam <shivam1634.cse18@chitkara.edu.in>
2021-03-03 14:58:21 +0530

    Create Reverse a string using Stack.java

---
+++ Stacks/Reverse a string using Stack.java
@@ -0,0 +1,23 @@
+/* isFull(), isEmpty(), push(int item), pop();
+Above functions are available to you for use on stack, So just complete the below function as per specifications. */
+
+static String reverseString(CQStack s, String st)
+{
+  int len=st.length();  
+  s=new CQStack();
+  char[] arr=new char[len];
+  for(int i=0;i<len;i++)
+  {
+    char ch=st.charAt(i);
+    int j=ch;
+    s.push(j);
+  }
+  for(int i=0;i<len;i++)
+  {
+    int j=s.pop();
+    char ch=(char)j;
+    arr[i]=ch;
+  }
+  st=new String(arr);
+  return st;
+}
