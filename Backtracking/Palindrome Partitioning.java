9092343f94477466f44481a2bff528df23a943bc
Shivam <shivam.rana1432@gmail.com>
2026-02-11 02:04:01 +0530

    Create Palindrome Partitioning.java
    Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
    
     
    
    Example 1:
    
    Input: s = "aab"
    Output: [["a","a","b"],["aa","b"]]
    Example 2:
    
    Input: s = "a"
    Output: [["a"]]

---
+++ Backtracking/Palindrome Partitioning.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(0, s, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, String s, List<String> path, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int end = start + 1; end <= s.length(); end++) {
+            String sub = s.substring(start, end);
+            if (isPalindrome(sub)) {
+                path.add(sub);
+                backtrack(end, s, path, result);
+                path.remove(path.size() - 1); // backtrack
+            }
+        }a
+    }
+
+    private boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
