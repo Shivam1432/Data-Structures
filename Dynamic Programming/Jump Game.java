2afffff20a9185d7d4819a30dddcf5614825e64b
Shivam <shivam.rana1432@gmail.com>
2026-02-16 03:24:47 +0530

    Update Jump Game.java

--- Dynamic Programming/Jump Game.java
+++ Dynamic Programming/Jump Game.java
@@ -1,3 +1,4 @@
+//Solution 1
 class Solution {
     public boolean canJump(int[] nums) {
         int i=0,m=nums.length-1;
@@ -9,5 +10,19 @@ class Solution {
             i=Math.max(i,j+jump);
         }
         return true;
+    }
+}
+//Solution 2
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0; 
     }
 }
