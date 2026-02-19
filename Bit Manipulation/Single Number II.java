e2c29987f259ca89c1dbfd29055df4a77d15875e
Shivam <shivam.rana1432@gmail.com>
2026-02-20 00:10:25 +0530

    Create Single Number II.java
    Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
    
    You must implement a solution with a linear runtime complexity and use only constant extra space.
    
     
    
    Example 1:
    
    Input: nums = [2,2,3,2]
    Output: 3
    Example 2:
    
    Input: nums = [0,1,0,1,0,1,99]
    Output: 99

---
+++ Bit Manipulation/Single Number II.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+
+    for (int i = 0; i < 32; ++i) {
+      int sum = 0;
+      for (final int num : nums)
+        sum += num >> i & 1;
+      sum %= 3;
+      ans |= sum << i;
+    }
+
+    return ans;
+    }
+}
+
+//Solution 2
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        
+        return -1;
+    }
+}
